8fa41c860fdb4d0beddca2de31c0bce3722e5ff0
Juanan <Juanan@Deinonychus>
2014-01-24 18:24:00 +0100

    Prueba viernes 18:23 maven2

---
+++ maven2/src/main/java/com/josito/maven2/Saludo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josito.maven2;
+
+/**
+ *
+ * @author Juanan
+ */
+public class Saludo {
+    
+    public Saludo(){};
+    
+    public void saludar(){
+        System.out.println("Hola majete!!!");
+    }
+}
