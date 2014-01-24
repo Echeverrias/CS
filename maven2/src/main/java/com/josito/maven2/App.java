84b2b1b7c00449755d7738430e46e5df0a606eda
Juanan <Juanan@Deinonychus>
2014-01-24 18:23:27 +0100

    Prueba viernes 18:23 maven2

---
+++ maven2/src/main/java/com/josito/maven2/App.java
@@ -0,0 +1,14 @@
+package com.josito.maven2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Saludo expresion= new Saludo();
+        expresion.saludar();
+    }
+}
