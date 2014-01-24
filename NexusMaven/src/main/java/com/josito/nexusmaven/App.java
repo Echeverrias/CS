17508248ad12cfb8c03ccc05a70ea5f65581dd3b
Juanan <Juanan@Deinonychus>
2014-01-24 21:27:54 +0100

    Prueba viernes 21:27 nexusmaven

---
+++ NexusMaven/src/main/java/com/josito/nexusmaven/App.java
@@ -0,0 +1,15 @@
+package com.josito.nexusmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!\n" );
+        Saludo expresion= new Saludo();
+        expresion.saludar();
+    }
+}
---
+++ NexusMaven/src/main/java/com/josito/nexusmaven/Saludo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josito.nexusmaven;
+
+/**
+ *
+ * @author Juanan
+ */
+public class Saludo {
+    public Saludo(){};
+    
+    public void saludar(){
+        System.out.println("Hola majete!!!");
+    }
+    
+     public void maldecir(){
+        System.out.println("Me cago en to!!!");
+    }
+     
+     public void despedir(){
+        System.out.println("Adios majete!!!");
+    }
+}
