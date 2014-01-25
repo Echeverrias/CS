7f490421aeea9be44589bdf94d61929eb15b540a
Juanan <Juanan@Deinonychus>
2014-01-25 10:38:32 +0100

    Audrey sabado 10:38

---
+++ Audrey/src/main/java/com/josito/audrey/App.java
@@ -0,0 +1,15 @@
+package com.josito.audrey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Hepburn ella=new Hepburn();
+        ella.saludar();
+        
+    }
+}
---
+++ Audrey/src/main/java/com/josito/audrey/Hepburn.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josito.audrey;
+
+/**
+ *
+ * @author Juanan
+ */
+public class Hepburn {
+    
+    public Hepburn(){
+        ;
+    }
+    
+    public void saludar(){
+        System.out.println("Hola");
+    }
+    
+}
