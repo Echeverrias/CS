abdf18c53d8476136aaef3beca028b2ca9bdf84a
Juanan <Juanan@Deinonychus>
2014-01-25 09:43:14 +0100

    Wendigo sbado 9:42

---
+++ Wendigo/src/main/java/com/josito/wendigo/App.java
@@ -0,0 +1,14 @@
+package com.josito.wendigo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Monstruo hombreNieve= new Monstruo();
+        hombreNieve.gritar();
+    }
+}
---
+++ Wendigo/src/main/java/com/josito/wendigo/Monstruo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josito.wendigo;
+
+/**
+ *
+ * @author Juanan
+ */
+public class Monstruo {
+    
+    public Monstruo(){
+        ;
+    }
+    
+    public void gritar(){
+        System.out.println("AAAAHHHHH!!!!");
+    }
+    
+}
