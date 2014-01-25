186aece60359678c175f3932ae3bd971975f54e4
Juanan <Juanan@Deinonychus>
2014-01-25 13:24:39 +0100

    sabado 13:24

---
+++ Zombie/src/main/java/com/josito/zombie/Caminante.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josito.zombie;
+
+/**
+ *
+ * @author Juanan
+ */
+public class Caminante {
+    
+    public Caminante (){;}
+    
+    public void gemir(){
+        System.out.println("aaaaggghhh");
+    }
+}
