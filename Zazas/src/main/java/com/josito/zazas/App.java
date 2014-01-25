4480c5d6ccfa76f7844948249afe979b5412f2ab
Juanan <Juanan@Deinonychus>
2014-01-25 15:03:27 +0100

    sabado 13:24

---
+++ Zazas/src/main/java/com/josito/zazas/App.java
@@ -0,0 +1,13 @@
+package com.josito.zazas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
